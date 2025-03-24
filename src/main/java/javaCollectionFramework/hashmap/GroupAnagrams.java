e466bfaf4633a5c96b58f200e11bf61828d911c7
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2025-03-24 15:51:11 +0530

    HashSet [Group Anagarms]

---
+++ src/main/java/javaCollectionFramework/hashmap/GroupAnagrams.java
@@ -0,0 +1,25 @@
+package javaCollectionFramework.hashmap;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagramMap = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            anagramMap.putIfAbsent(sortedStr, new ArrayList<>());
+            anagramMap.get(sortedStr).add(str);
+        }
+
+        return new ArrayList<>(anagramMap.values());
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println("Grouped Anagrams: " + groupAnagrams(words));
+    }
+}
